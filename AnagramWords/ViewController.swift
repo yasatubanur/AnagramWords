2a9f9100bbd14d2d8f2456cd1bae939c8640671a
Tuba Nur <yasatubanur@gmail.com>
2022-03-07 16:50:08 +0300

    prompt for answer created

--- AnagramWords/ViewController.swift
+++ AnagramWords/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         navigationController?.navigationBar.prefersLargeTitles = true
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
@@ -47,6 +49,24 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
         cell.textLabel?.text = usedWords[indexPath.row]
         return cell
+    }
+    
+    @objc func promptForAnswer(){
+        let ac = UIAlertController(title: "Enter Answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default){
+            [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac,animated: true)
+    }
+    
+    func submit(_ answer: String){
+        
     }
 }
 
