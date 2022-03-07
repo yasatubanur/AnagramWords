3e2fc79ffdb3636ca1aaf9da15b9795ab26ecf28
Tuba Nur <yasatubanur@gmail.com>
2022-03-07 23:14:12 +0300

    isPossible function edited

--- AnagramWords/ViewController.swift
+++ AnagramWords/ViewController.swift
@@ -80,6 +80,15 @@ class ViewController: UITableViewController {
     }
     
     func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else { return false }
+        
+        for letter in word{
+            if let position = tempWord.firstIndex(of: letter){
+                tempWord.remove(at: position)
+            }else{
+                return false
+            }
+        }
         return true
     }
     
