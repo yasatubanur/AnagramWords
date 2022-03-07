62f847b49aec13ce29f4ecc1bd0b8a217a87fb52
Tuba Nur <yasatubanur@gmail.com>
2022-03-07 23:32:36 +0300

    else statements added

--- AnagramWords/ViewController.swift
+++ AnagramWords/ViewController.swift
@@ -67,6 +67,9 @@ class ViewController: UITableViewController {
     func submit(_ answer: String){
         let lowerAnswer = answer.lowercased()
         
+        let errorTitle: String
+        let errorMessage: String
+        
         if isPossible(word: lowerAnswer){
             if isOriginal(word: lowerAnswer){
                 if isReal(word: lowerAnswer){
@@ -74,9 +77,25 @@ class ViewController: UITableViewController {
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                }else{
+                    errorTitle = "Word not recognized"
+                    errorMessage = "You can't just make them up, you know!"
                 }
+            }else{
+                errorTitle = "Word used already!"
+                errorMessage = "Be more orginal !"
             }
+        }else{
+            guard let title = title?.lowercased() else { return }
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title)"
         }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac,animated: true)
     }
     
     func isPossible(word: String) -> Bool {
